153c74eddf35dd484f37f6ddbc162d1681f6903f
timothymoore <timothymoore@localhost>
2022-03-08 16:41:32 -0500

    vertex header?

---
+++ 244-project/vertex.h
@@ -0,0 +1,16 @@
+/*
+ * vertex.h
+ *
+ *  Created on: Mar. 8, 2022
+ *      Author: timothymoore
+ */
+
+#ifndef VERTEX_H_
+#define VERTEX_H_
+
+class vertex {
+	int data;
+};
+
+
+#endif /* VERTEX_H_ */
