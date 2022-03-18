33ba968f9c50057b7d3a976e76415944bba649c0
timothymoore <timothymoore@localhost>
2022-03-18 00:56:41 -0400

    Uploading all my A3 files...

---
+++ 244-project/Edge.h
@@ -0,0 +1,28 @@
+/*
+ * Edge.h
+ *
+ *  Created on: Mar. 11, 2022
+ *      Author: timothymoore
+ */
+
+#ifndef EDGE_H_
+#define EDGE_H_
+
+#include "Vertex.h"
+
+class Edge {
+private:
+	Vertex* source;
+	Vertex* dest;
+	int weight;
+public:
+	Edge(Vertex*, Vertex*, int = 0);
+	Vertex* getSource() const;
+	Vertex* getDest() const;
+	int getWeight() const;
+	void setWeight(const int);
+	void print() const;
+};
+
+
+#endif /* EDGE_H_ */
---
+++ 244-project/Graph.cpp
@@ -0,0 +1,58 @@
+/*
+ * Graph.cpp
+ *
+ *  Created on: Mar. 12, 2022
+ *      Author: timothymoore
+ */
+
+#include "Graph.h"
+
+Graph::Graph() {
+	nodes = nullptr;
+	edges = nullptr;
+	no_vertices = 0;
+	no_edges = 0;
+}
+
+Graph::~Graph() {}
+
+size_t Graph::getNoVertices() const {
+	return no_vertices;
+}
+
+size_t Graph::getNoEdges() const {
+	return no_edges;
+}
+
+Vertex** Graph::getNodes() const {
+	return nodes;
+}
+
+Edge** Graph::getEdges() const {
+	return edges;
+}
+
+ostream& operator<<(ostream &output, const Graph &g) {
+	if (g.no_vertices == 0) {
+		output << "V = empty set\nE = empty set" << endl;
+		return output;
+	}
+	output << "V = {";
+	for (int i = 0; i < g.no_vertices - 1; ++i)
+		output << g.nodes[i]->getValue() << ", ";
+
+	output << g.nodes[g.no_vertices - 1]->getValue() << "}" << endl;
+
+	if (g.no_edges == 0) {
+		output << "E = empty set" << endl;
+		return output;
+	}
+	output << "E = {";
+	for (int i = 0; i < g.no_edges - 1; ++i) {
+		g.edges[i]->print();
+		output << ", ";
+	}
+	g.edges[g.no_edges - 1]->print();
+	output << "}" << endl;
+	return output;
+}
