9c1aa6c5246b9a819ec67428af35832bf5475e9d
timothymoore <timothymoore@timothys-mbp.wireless.concordia.ca>
2022-04-08 14:39:32 -0400

    More changes

---
+++ 244-project/UndirectedGraph.h
@@ -0,0 +1,30 @@
+/*
+ * UndirectedGraph.h
+ *
+ *  Created on: Apr. 8, 2022
+ *      Author: timothymoore
+ */
+
+#ifndef UNDIRECTEDGRAPH_H_
+#define UNDIRECTEDGRAPH_H_
+
+#include "Graph.h"
+
+class UndirectedGraph : public Graph {
+public:
+	UndirectedGraph();
+	UndirectedGraph(const UndirectedGraph&);
+	virtual bool addVertex(Vertex &);
+	virtual bool addVertices(Vertex*, int);
+	virtual bool searchVertex(const Vertex&);
+	virtual bool searchEdge(const Edge&);
+	virtual bool removeVertex(Vertex&);
+	virtual void display() const;
+	virtual bool addEdge(Edge&);
+	virtual bool remove(Edge&);
+	virtual string toString() const;
+
+
+};
+
+#endif /* UNDIRECTEDGRAPH_H_ */
