a1242ffab25ac1a624373a526b53d53297a2bcc3
timothymoore <timothymoore@localhost>
2022-04-19 13:13:26 -0400

    undirected getpaths

--- 244-project/UndirectedGraph.cpp
+++ 244-project/UndirectedGraph.cpp
@@ -42,4 +42,50 @@ bool UndirectedGraph::addVertex(Vertex& v) {
 	return true;
 }
 
+vector<vector<Edge> > DirectedGraph::getPaths(Vertex<int> &v,
+		unordered_map<int, bool> is_visited) {
+	// cout << "Calling getPaths for node " << v.getValue() << endl;
+	unordered_map<int, bool> new_visited = is_visited;
+	new_visited[v.getValue()] = true;
+	vector<vector<Edge> > paths;
+	vector<Edge> pt;
+	for (int i = 0; i < no_edges; ++i) {
+		if (edges[i]->getSource()->getValue() == v.getValue()
+				&& new_visited[edges[i]->getDest()->getValue()] == false) {
+			//new_visited[edges[i]->getDest()->getValue()] = true;
+			pt.push_back(*edges[i]);
+			paths.push_back(pt);
+			vector<vector<Edge> > childPaths = getPaths(*edges[i]->getDest(),
+					new_visited);
+			// if (!childPaths.isEmpty())
+			for (int j = 0; j < childPaths.size(); ++j) {
+				for (Edge e : childPaths[j]) pt.push_back(e);
+				paths.push_back(pt);
+				pt.clear();
+				pt.push_back(*edges[i]);
+			}
+			pt.clear();
+		} else if (edges[i]->getDest()->getValue() == v.getValue()
+				&& new_visited[edges[i]->getSource()->getValue()] == false) {
+			//new_visited[edges[i]->getSource()->getValue()] = true;
+			Edge e(edges[i]->getDest(), edges[i]->getSource(), edges[i]->getWeight());
+			pt.push_back(e);
+			paths.push_back(pt);
+			vector<vector<Edge> > childPaths = getPaths(*edges[i]->getSource(),
+					new_visited);
+			// if (!childPaths.isEmpty())
+			for (int j = 0; j < childPaths.size(); ++j) {
+				for (Edge e : childPaths[j]) pt.push_back(e);
+				paths.push_back(pt);
+				pt.clear();
+				pt.push_back(e);
+			}
+			pt.clear();
+		}
+	}
+	return paths;
+}
+
+vector< vector<Edge> > getPaths(Vertex<int>&, unordered_map<int, bool>);
+
 
